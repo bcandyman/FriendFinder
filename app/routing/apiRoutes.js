53542c25512cc597d33086ec7ec91e39b7effd0b
Benjamin Candy <bcandyman93@gmail.com>
2019-12-16 14:42:50 -0800

    Added initial routings

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,13 @@
+var friends = require('../data/friends.js')
+
+module.exports = function (app) {
+
+    app.get("/", function (req, res) {
+
+    });
+
+    app.post('/', function (req, res) {
+
+    });
+}
+
