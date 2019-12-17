48c689025ead1de234b3cd674fc447f18622625c
Benjamin Candy <bcandyman93@gmail.com>
2019-12-16 22:15:23 -0800

    Changed template friend object to json format.

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,8 +1,8 @@
 let profiles = [
     {
-        name: "server Name",
-        photoAddress: "http://something",
-        scores: [1, 2, 3, 4, 5, 4, 3, 4, 5, 2]
+        'name': 'server Namer',
+        'photoAddress': 'http://something',
+        'scores': ['1', '2', '3', '4', '5', '4', '3', '4', '5', '2']
     }
 ]
 
