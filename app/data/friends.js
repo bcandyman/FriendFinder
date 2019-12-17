3c5e0d24160a28231bf8f5bf9db603fb704362b3
Benjamin Candy <bcandyman93@gmail.com>
2019-12-16 16:43:15 -0800

    Added initial profile object

---
+++ app/data/friends.js
@@ -0,0 +1,9 @@
+let profiles = [
+    {
+        name: "server Name",
+        photoAddress: "http://something",
+        scores: [1, 2, 3, 4, 5, 4, 3, 4, 5, 2]
+    }
+]
+
+module.exports = profiles
