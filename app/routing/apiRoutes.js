2b3cb50d8e962f4ef1ccacf5877413c56a2b6816
Benjamin Candy <bcandyman93@gmail.com>
2019-12-16 22:25:38 -0800

    Changed friends variable to profiles.

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -3,14 +3,14 @@ var profiles = require('../data/friends.js')
 module.exports = function (app) {
 
     app.get("/api/profiles", function (req, res) {
-        return res.json(profiles);
+        res.json(profiles);
     });
 
     app.post('/survey', function (req, res) {
 
-        friends.push(req.body)
-        console.log(friends)
-        res.json(friends);
+        profiles.push(req.body)
+        console.log(profiles)
+        res.json(profiles);
     });
 }
 
