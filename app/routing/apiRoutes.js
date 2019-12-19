1efbaec33d2b77a19b3e141efe42e34a14eae81a
Benjamin Candy <bcandyman93@gmail.com>
2019-12-18 23:33:27 -0800

    Added logic to determine matched friend

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,5 +1,32 @@
 var profiles = require('../data/friends.js')
 
+const getScore = (profile) => {
+    let score = 0;
+
+    profile.scores.forEach((val) => {
+        score += parseInt(val);
+    })
+
+    return score;
+}
+
+let findFriend = (userProfile) => {
+
+    const myScore = getScore(userProfile);
+    let closestFriend = profiles[0];
+
+    for (let i = 0; i < profiles.length - 1; i++) {
+        let currentDiff = Math.abs(myScore - getScore(closestFriend));
+        let proposedDiff = Math.abs(myScore - getScore(profiles[i]));
+
+        if (proposedDiff < currentDiff) {
+            closestFriend = profiles[i]
+        }
+    }
+    return closestFriend
+}
+
+
 module.exports = function (app) {
 
     app.get("/api/profiles", function (req, res) {
@@ -7,10 +34,10 @@ module.exports = function (app) {
     });
 
     app.post('/survey', function (req, res) {
+        const userProfile = req.body;
+        profiles.push(userProfile);
+        const yourFriend = findFriend(userProfile);
 
-        profiles.push(req.body)
-        console.log(profiles)
-        res.json(profiles);
+        res.json(yourFriend);
     });
 }
-
