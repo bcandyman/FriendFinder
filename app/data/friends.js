4252396797b78053a05ea122fab1aa35732674ea
Benjamin Candy <bcandyman93@gmail.com>
2019-12-18 23:30:19 -0800

    Added additional profiles to the profiles object for testing.

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,8 +1,18 @@
 let profiles = [
     {
-        'name': 'server Namer',
-        'photoAddress': 'http://something',
+        'name': 'Jenny',
+        'photoAddress': '1218191.jpg',
         'scores': ['1', '2', '3', '4', '5', '4', '3', '4', '5', '2']
+    },
+    {
+        'name': 'Jose',
+        'photoAddress': '1218192.jpg',
+        'scores': ['4', '1', '3', '4', '2', '4', '3', '4', '5', '2']
+    },
+    {
+        'name': 'Susan',
+        'photoAddress': '1218193.jpg',
+        'scores': ['2', '1', '3', '4', '2', '2', '3', '4', '1', '2']
     }
 ]
 
