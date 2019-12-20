a116c4c87ee2589f1a139fcf49c7ceba6af085b7
Benjamin Candy <bcandyman93@gmail.com>
2019-12-19 23:29:05 -0800

    Added comments

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
+//Initialize
 const express = require('express');
-
 const app = express();
-
 const PORT = process.env.PORT || 3000;
+
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('app/public/assets/'))
 
+//require external routing files
 require("./app/routing/apiRoutes")(app);
 require("./app/routing/htmlRoutes")(app);
+
 
 app.listen(PORT, function () {
     console.log('App listening on PORT ' + PORT);
