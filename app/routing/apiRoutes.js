616bc95c7aed0e0778c07f5d4e8e08c2ef344c85
Benjamin Candy <bcandyman93@gmail.com>
2019-12-19 23:44:28 -0800

    Added comments

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -26,13 +26,13 @@ const findFriend = (userProfile) => {
 
     //set initial difference between user and default friend
     let currentDiff = Math.abs(myScore - getScore(closestMatch));
-    
+
     /*loop through friends array, testing if proposed
     friend is a better match than the current match.*/
     for (let i = 0; i < friends.length - 1; i++) {
-        
+
         /*set proposedDiff to score difference between
-        user and proposed friend*/ 
+        user and proposed friend*/
         let proposedDiff = Math.abs(myScore - getScore(friends[i]));
 
         //if the proposed friend is a closer match to the user
@@ -47,18 +47,29 @@ const findFriend = (userProfile) => {
 }
 
 
+
 //export to make available to server.js
 module.exports = function (app) {
+
 
     app.get("/api/profiles/all", function (req, res) {
         res.json(friends);
     });
 
+
+
     app.post('/survey', function (req, res) {
+
+        //set user data to userProfile
         const userProfile = req.body;
+
+        //add user data to friends array
         friends.push(userProfile);
+
+        //find the closest match to the user
         const yourFriend = findFriend(userProfile);
 
+        //return the closest match to the current user
         res.json(yourFriend);
     });
 }
