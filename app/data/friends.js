156af65c602dec4b1953bae39178573c25c74e06
Benjamin Candy <bcandyman93@gmail.com>
2019-12-20 21:54:32 -0800

    Added gender key into profile object in an attempt to keep male names with male images and vice versa.

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,17 +1,20 @@
 let profiles = [
     {
         'name': 'Jenny',
-        'photoAddress': '1218191.jpg',
+        'photoAddress': '',
+        'gender':"f",
         'scores': ['1', '2', '3', '4', '5', '4', '3', '4', '5', '2']
     },
     {
         'name': 'Jose',
-        'photoAddress': '1218192.jpg',
+        'photoAddress': '',
+        'gender':'m',
         'scores': ['4', '1', '3', '4', '2', '4', '3', '4', '5', '2']
     },
     {
         'name': 'Susan',
-        'photoAddress': '1218193.jpg',
+        'photoAddress': '',
+        'gender':'f',
         'scores': ['2', '1', '3', '4', '2', '2', '3', '4', '1', '2']
     }
 ]
